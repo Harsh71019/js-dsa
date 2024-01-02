0325a7a567e182cc8faa3e0a93b06ab57c2864e3
Harsh Patel <harsh710patel@gmail.com>
2024-01-03 00:42:47 +0530

    solution to cyclic problem

--- Array/cyclic_array.js
+++ Array/cyclic_array.js
@@ -1,5 +1,13 @@
 const cycleArray = [3, 4, 5, 6, 7];
 
-function cycle(arr, k) {}
+function cycle(arr, k) {
+  let result = new Array(arr.length);
+
+  for (let i = 0; i < result.length; i++) {
+    result[(i + k) % arr.length] = arr[i];
+  }
+
+  return result;
+}
 
 console.log(cycle(cycleArray, 3));
